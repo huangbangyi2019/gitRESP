5c78b43a80ca019c5e9366771566b10955c8344f
huangbangyi2019 <10094476088@qq.com>
2020-06-10 10:25:09 +0800

    对用户模块进行DDL语句的操作

---
+++ git-project/src/com/git/dao/UserDao.java
@@ -0,0 +1,23 @@
+package com.git.dao;
+
+import java.util.List;
+
+public class UserDao {
+
+	public void addUser(int id, String usrename,String password) {
+		//添加操作
+	}
+	
+	public void deleteUser(int id) {
+		//删除操作
+	}
+	
+	public void updateUser(int id, String newUsername, String newPassword) {
+		// 修改操作
+	}
+	
+	public List<Object> getUsers (int pageIndex, int pageSize) {
+		// 获取操作
+		return null;
+	}
+}
